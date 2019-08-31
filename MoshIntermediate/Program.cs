efcc31d9f3fcddd4974e0a6e8a546bc7180973af
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-30 21:50:20 -0400

    indexers1 done

--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -6,29 +6,13 @@ using System.Threading.Tasks;
 
 namespace MoshIntermediate
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var person = Person.Parse("Tom");
-            person.Introduce("maddie");
+            var cookie = new HttpCookie();
+            cookie["name"] = "Tom";
+            Console.WriteLine(cookie["name"]);
         }
     }
 }
