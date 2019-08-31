e068b08a7e6dc6e124c8947205021dae9a717a46
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-31 00:20:32 -0400

    fields is done

---
+++ MoshIntermediate/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace MoshIntermediate
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        //this is the same as initializing list in empty constructor
+        //use readonly so the list cannot be reinitialized and lost
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            //the below line errors due to readonly  
+            //Orders = new List<Order>();
+        }
+    }
+}
--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,58 +7,15 @@ namespace MoshIntermediate
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
 
-            try
-            {
-                var num = int.Parse("abc");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
 
-            int number;
-            //tryparse returns true or false
-            var result = int.TryParse("abc", out number);
-            if (result)
-            {
-                Console.WriteLine(number);
-            }
-            else
-            {
-                Console.WriteLine("failed to convert");
-            }
-        }
 
-        static void UseParams()
-        {
-            var calculator = new Calculator();
-            Console.WriteLine(calculator.Add(1, 2));
-            Console.WriteLine(calculator.Add(1, 2, 3));
-            Console.WriteLine(calculator.Add(1, 2, 3, 4));
-            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4 }));
-        }
-
-        static void UsePoints()
-        {
-            try
-            {
-                var point = new Point(10, 20);
-                point.Move(new Point(40, 60));
-                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
-
-                point.Move(100, 200);
-                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
+            Console.WriteLine(customer.Orders.Count);
         }
     }
 }
