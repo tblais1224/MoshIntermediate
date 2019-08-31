56900ecdf1e22bc417a957ca42dea2eea583f6c3
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-30 21:35:09 -0400

    classes 1 done

--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace MoshIntermediate
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var person = Person.Parse("Tom");
+            person.Introduce("maddie");
         }
     }
 }
