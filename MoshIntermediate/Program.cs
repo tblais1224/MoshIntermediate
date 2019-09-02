535c82346de07bd09fb94731826d7dbac24813b0
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-02 16:49:09 -0400

    composition 1 done

--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -10,9 +10,13 @@ namespace MoshIntermediate
     {
         static void Main(string[] args)
         {
-            var text = new Text();
-            text.Width = 100;
-            text.Copy();
+            var dbMigrator = new DbMigrator(new Logger());
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
 }
