84291728106351bea694df86f893dd88bebc891e
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-02 15:02:48 -0400

    stack overflow post exercise is done

--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -10,15 +10,12 @@ namespace MoshIntermediate
     {
         static void Main(string[] args)
         {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            //tested ten second timer
-            do
-            {
-                Console.WriteLine(stopwatch.Stop());
-            } while (stopwatch.Stop() < TimeSpan.FromSeconds(10));
-
+            var post = new Post();
+            post.MakePost("Patriots Win", "the patriots win another superbowl behind the genius of belicheck and the excellence of tom brady.");
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.GetPost();
         }
     }
 }
