2888d846f6199e7b675e18b9980e934b0c9723ba
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-03 10:50:12 -0400

    constuctor inheritence with base is done

--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -3,26 +3,14 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
-using Amazon;
 
 namespace MoshIntermediate
 {
-    // public class GoldCustomer : Customer
-    // {
-    //   public void OfferVoucher()
-    // {
-    //   var rating = this.CalculateRating(true);
-
-    //}
-    //}
     class Program
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            //ratecalc turned to internal
-           // Amazon.RateCalculator calculator = new RateCalculator();
-
+            var car = new Car("ABC123");
         }
     }
 }
---
+++ MoshIntermediate/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MoshIntermediate
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+        public Vehicle()
+        {
+            Console.WriteLine("vehicle is being initialized");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine("vehicle is being initialized. {0}", registrationNumber);
+        }
+    }
+}
