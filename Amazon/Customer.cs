4e9090127dbbdbd764826fed5525f8a13f6af43f
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-03 10:37:51 -0400

    access modifier done

---
+++ Amazon/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            //  var rating = CalculateRating(true);
+            // Console.WriteLine(rating == 0 ? "level 1 promotion" : "level 2 promotion");
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+
+            Console.WriteLine("promote logic changed");
+        }
+        //protected int CalculateRating(bool excludeOrders)
+        //{
+        //    return 0;
+        //}
+    }
+}
--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -3,20 +3,26 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon;
 
 namespace MoshIntermediate
 {
+    // public class GoldCustomer : Customer
+    // {
+    //   public void OfferVoucher()
+    // {
+    //   var rating = this.CalculateRating(true);
+
+    //}
+    //}
     class Program
     {
         static void Main(string[] args)
         {
-            var dbMigrator = new DbMigrator(new Logger());
-            var logger = new Logger();
-            var installer = new Installer(logger);
+            var customer = new Customer();
+            //ratecalc turned to internal
+           // Amazon.RateCalculator calculator = new RateCalculator();
 
-            dbMigrator.Migrate();
-
-            installer.Install();
         }
     }
 }
