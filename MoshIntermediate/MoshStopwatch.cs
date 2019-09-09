8b21c7ef8e4368de5b3f8531baa26fb2a3288bdb
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-09 13:32:58 -0400

    reviewed moshs code for stopwatch challenge

---
+++ MoshIntermediate/MoshStopwatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MoshIntermediate
+{
+    public class MoshStopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        private bool _running = false;
+
+        public void Start()
+        {
+            if (_running)
+            {
+                throw new InvalidOperationException("stopwatch already running");
+            }
+            _startTime = DateTime.Now;
+            _running = true;
+
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+            {
+                throw new InvalidOperationException("stopwatch is not running");
+            }
+            _endTime = DateTime.Now;
+            _running = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+
+    }
+}
--- MoshIntermediate/Program.cs
+++ MoshIntermediate/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MoshIntermediate
@@ -10,15 +11,35 @@ namespace MoshIntermediate
     {
         static void Main(string[] args)
         {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            //my code
 
-            //tested ten second timer
-            do
+            //var stopwatch = new Stopwatch();
+            //stopwatch.Start();
+
+            ////tested ten second timer
+            //do
+            //{
+            //    Console.WriteLine(stopwatch.Stop());
+            //} while (stopwatch.Stop() < TimeSpan.FromSeconds(10));
+
+
+
+            // mosh stopwatch review code
+            var moshStopwatch = new MoshStopwatch();
+
+            for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine(stopwatch.Stop());
-            } while (stopwatch.Stop() < TimeSpan.FromSeconds(10));
+                moshStopwatch.Start();
+                Thread.Sleep(1000);
 
+                moshStopwatch.Stop();
+
+                Console.WriteLine("Duration: " + moshStopwatch.GetInterval().ToString());
+
+                Console.WriteLine("Press enter to run the stopwatch once more.");
+
+                Console.ReadLine();
+            }
         }
     }
 }
